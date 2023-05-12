62760f0347255d78c2c532fc03d1313a7c6c8081
park jun hyeon <dlsnditk90@naver.com>
2023-05-12 09:41:47 +0900

    20230512

---
+++ 백준/src/jh05/EX01.java
@@ -0,0 +1,16 @@
+package jh05;
+
+import java.util.Scanner;
+
+public class EX01 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.nextLine();
+		int b = scan.nextInt();
+		
+		char c = a.charAt(b-1);
+		
+		System.out.println(c);
+	}
+}
---
+++ 백준/src/jh05/EX02.java
@@ -0,0 +1,15 @@
+package jh05;
+
+import java.util.Scanner;
+
+public class EX02 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String a = scan.nextLine();
+		
+		int b = a.length();
+		
+		System.out.println(b);
+	}
+}
